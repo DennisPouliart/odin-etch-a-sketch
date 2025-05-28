02611998f0dc53b6d249c05b5eddab6301f47716
DennisPouliart <dennis14@live.be>
2025-05-28 23:28:10 +0000

    Added hover effect on squares using eventlisteners

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-console.log("test");
-
+const container = document.querySelector('#container');
 
 for(i=0; i<256; i++) {
-const container = document.querySelector('#container')
-const square = document.createElement("div");
-container.appendChild(square);
-console.log("test");
+    const square = document.createElement("div");
+    square.setAttribute("id", "box");
+    container.appendChild(square);
+    console.log("test");
 }
+
+const box = document.querySelectorAll('#box');
+
+console.log(box);
+
+box.forEach(box => {
+    box.addEventListener("mouseover", (event) => {
+        console.log("event fired");
+        event.target.style.backgroundColor = "purple";
+    }) 
+});
+
+/*box.addEventListener("mouseover", (event) => {
+    console.log("event fired");
+    event.target.style.backgroundColor = "purple";
+}) */
