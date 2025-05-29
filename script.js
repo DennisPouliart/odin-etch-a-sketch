0e87aef6a60ef4c5f1e680c89390b3709092fb31
DennisPouliart <dennis14@live.be>
2025-05-29 20:29:41 +0000

    Reworked logic to print new grid

--- script.js
+++ script.js
@@ -9,9 +9,20 @@ for(i=0; i<256; i++) {
     console.log("test");
 }
 
-const box = document.querySelectorAll('#box');
+const box = boxSelection();
 
-console.log(box);
+function boxSelection() {
+    return document.querySelectorAll('#box');
+}
+
+function mouseLeave() {
+    box.forEach(box => {
+        box.addEventListener("mouseleave", (event) => {
+            console.log("mouse left");
+            event.target.classList.add("active");
+        }) 
+    });
+};
 
 box.forEach(box => {
     box.addEventListener("mouseenter", (event) => {
@@ -20,10 +31,49 @@ box.forEach(box => {
     }) 
 });
 
-reset.addEventListener("click", function (e) {
+reset.addEventListener("click", function () {
     console.log("reset");
     box.forEach((box) => {
         box.classList.remove("active");
     })
 });
 
+change.addEventListener("click", function () {
+    console.log("change");
+    //let size = window.prompt("Enter new grid size", "0x0");
+    let existingBoxes = document.querySelector('#container');
+    let child = existingBoxes.lastElementChild;
+
+    /* while (child) {
+        existingBoxes.removeChild(child);
+        child = existingBoxes.lastElementChild;
+    } */
+
+        for(x=256; x>0; x--) {
+            existingBoxes.removeChild(child);
+            child = existingBoxes.lastElementChild;
+        }
+
+    test();
+
+});
+
+function test() {
+
+    for(z=0; z<256; z++) {
+        const op = document.createElement("div");
+        op.setAttribute("id", "box");
+        container.appendChild(op);
+        console.log("test 2");
+        
+    }
+
+    const bow = document.querySelectorAll('#box');
+
+    bow.forEach(bow => {
+        bow.addEventListener("mouseenter", (event) => {
+            console.log("event enter fired 2");
+            event.target.classList.add("active");
+        }) 
+    });
+};
